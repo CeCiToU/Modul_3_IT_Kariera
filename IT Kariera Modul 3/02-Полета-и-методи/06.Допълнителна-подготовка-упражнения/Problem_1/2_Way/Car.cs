5a1ffa2f9f9eee583e306a1d469e080f7f0b9179
Tsvetoslav <cveti3060@gmail.com>
2022-06-20 23:50:14 +0300

    some exercise

---
+++ IT Kariera Modul 3/02-Полета-и-методи/06.Допълнителна-подготовка-упражнения/Problem_1/2_Way/Car.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace ConsoleApp308
+{
+    internal class Car
+    {
+        private string model;
+        private double fuel;
+        private double usedFuel;
+        private double kmInTotal;
+
+        public Car(string models, double fuels, double usedFuels)
+        {
+            Model = models;
+            Fuel = fuels;
+            usedFuel = usedFuels;
+        }
+
+        public string Model { get => model; set => model = value; }
+        public double Fuel { get => fuel; set => fuel = value; }
+        public double UsedFuel { get => usedFuel; set => usedFuel = value; }
+        public double KmInTotal { get => kmInTotal; set => kmInTotal = value; }
+
+        internal void Drive(double km)
+        {
+
+            if(fuel < km * usedFuel)
+            {
+                throw new ArgumentOutOfRangeException();
+            }
+            else
+            {
+                fuel -= km * usedFuel;
+                KmInTotal += km;
+            }
+        }
+
+        public override string ToString()
+        {
+            return $"{Model} {Fuel:F2} {KmInTotal}";
+        }
+    }
+}
---
+++ IT Kariera Modul 3/02-Полета-и-методи/06.Допълнителна-подготовка-упражнения/Problem_1/2_Way/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ConsoleApp308
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Car> cars = new List<Car>();
+            int numOfCars = int.Parse(Console.ReadLine());
+            for (int i = 0; i < numOfCars; i++)
+            {
+                string[] info = Console.ReadLine().Split().ToArray();
+                Car currentCar = new Car(info[0], double.Parse(info[1]), double.Parse(info[2]));
+                cars.Add(currentCar);
+            }
+            string[] command = Console.ReadLine().Split().ToArray();
+            while (command[0] != "End")
+            {
+                if (command[0] == "Drive")
+                {
+                    for (int i = 0; i < cars.Count; i++)
+                    {
+                        if (cars[i].Model == command[1])
+                        {
+                            try
+                            {
+                                cars[i].Drive(double.Parse(command[2]));
+
+                            }
+                            catch(ArgumentOutOfRangeException)
+                            {
+                                Console.WriteLine("Insufficient fuel for the drive!");
+                            }
+                            break;
+                        }
+                    }
+                }
+                command = Console.ReadLine().Split().ToArray();
+            }
+
+            Console.WriteLine(string.Join("\n", cars));
+        } 
+    }
+}
